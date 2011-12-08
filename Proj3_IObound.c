60a47bf49b44984415f73f29124dea2b9de68d67
spandya19 <sweta19@gmail.com>
2011-12-08 00:44:59 -0600

    Update Proj3_IObound.c

--- Proj3_IObound.c
+++ Proj3_IObound.c
@@ -7,9 +7,11 @@ int i,c;
 char ch;
 clock_t start,stop;
 double duration;
+//Opend one file to read from and two files to write in it
 fp=fopen("test.txt","r");
 ft=fopen("test1.txt","w");
 fs=fopen("test2.txt","w");
+//started clock to record teh execution time
 start=clock();
 for(c=1;c<25;c++)
 {
@@ -21,6 +23,7 @@ while(1)
 	if(ch==EOF)
 		break;
 	else
+	//put the every character of file test.txt to test1.txt and test2.txt
 		fputc(ch,ft);
 		fputc(ch,fs);
 }
@@ -29,7 +32,9 @@ while(1)
 fclose(fp);
 fclose(ft);
 fclose(fs);
+//stop the recording of time
 stop=clock();
+//calculating the duration to find the time taken for execution of this Input Output process
 duration=(double)(stop-start)/CLOCKS_PER_SEC;
 printf("\n Time elapsed for IObound Process is: %.8lf\n",duration);
 }
